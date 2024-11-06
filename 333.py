d0884d92a9bc7ec2cd14ad864b7cea7040f0239d
uid=0(wwwLui) <39214770+wwwLui@users.noreply.github.com>
2024-11-06 00:49:08 -0400

    Update 333.py

--- 333.py
+++ 333.py
@@ -2,8 +2,8 @@ from typing import List, Union, Generator, Iterator, Optional
 from pprint import pprint
 import requests, json, warnings
 
-# Uncomment to disable SSL verification warnings if needed.
-# warnings.filterwarnings('ignore', message='Unverified HTTPS request')
+
+warnings.filterwarnings('ignore', message='Unverified HTTPS request')
 
 class Pipeline:
     def __init__(self):
@@ -15,59 +15,30 @@ class Pipeline:
         # Please note that N8N do not support stream reponses
 
     async def on_startup(self):
-        # This function is called when the server is started.
         print(f"on_startup: {__name__}")
         pass
     
     async def on_shutdown(self): 
-        # This function is called when the server is shutdown.
         print(f"on_shutdown: {__name__}")
         pass
 
     async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
-        # This function is called before the OpenAI API request is made. You can modify the form data before it is sent to the OpenAI API.
-        print(f"inlet: {__name__}")
-        if self.debug:
-            print(f"inlet: {__name__} - body:")
-            pprint(body)
-            print(f"inlet: {__name__} - user:")
-            pprint(user)
         return body
 
     async def outlet(self, body: dict, user: Optional[dict] = None) -> dict:
-        # This function is called after the OpenAI API response is completed. You can modify the messages after they are received from the OpenAI API.
-        print(f"outlet: {__name__}")
-        if self.debug:
-            print(f"outlet: {__name__} - body:")
-            pprint(body)
-            print(f"outlet: {__name__} - user:")
-            pprint(user)
         return body
 
     def pipe(self, user_message: str, model_id: str, messages: List[dict], body: dict) -> Union[str, Generator, Iterator]:
-        # This is where you can add your custom pipelines like RAG.
         print(f"pipe: {__name__}")
-        
-        if self.debug:
-            print(f"pipe: {__name__} - received message from user: {user_message}")
-        
-        # This function triggers the workflow using the specified API.
+       
         headers = {
       #      'Authorization': f'Bearer {self.api_key}',
             'Content-Type': 'application/json'
         }
-        data = {
-            "inputs": {"prompt": user_message},
-            "user": body["user"]["email"]
-        }
-
         response = requests.get(self.api_url, headers=headers, verify=self.verify_ssl)
         if response.status_code == 200:
             response.text
             try:
-                yield   response.text
-            except json.JSONDecodeError as e:
-                print(f"Failed to parse JSON from line. Error: {str(e)}")
-                yield "Error in JSON parsing."
+                return response.content
         else:
-            yield f"Workflow request failed with status code: {response.status_code}"
+            return f"Workflow request failed with status code: {response.status_code}"

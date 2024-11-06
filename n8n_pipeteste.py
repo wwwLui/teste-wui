698b98e29f687cda8478eb6c6fb6fefbb4c6b3a6
uid=0(wwwLui) <39214770+wwwLui@users.noreply.github.com>
2024-11-05 23:21:12 -0400

    Update n8n_pipeteste.py

--- n8n_pipeteste.py
+++ n8n_pipeteste.py
@@ -7,7 +7,7 @@ import requests, json, warnings
 
 class Pipeline:
     def __init__(self):
-        self.name = "AIM - N8N Agent Pipeline"
+        self.name = "AIM - N8N Agent Pipeline2"
         self.api_url = "https://n8n.autointmind.com/webhook/62f78f96-6cae-4cfd-985d-27d2da8fd8b5"     # Set correct hostname
         self.api_key = "__n8n_BLANK_VALUE_e5362baf-c777-4d57-a609-6eaf1f9e87f6"                                    # Insert your actual API key here
         self.verify_ssl = True

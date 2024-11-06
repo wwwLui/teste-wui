92c17f8f0533c9cf0b70f5fbc2a4bc6ac37b3790
uid=0(wwwLui) <39214770+wwwLui@users.noreply.github.com>
2024-11-05 23:44:31 -0400

    Update n8n_pipeteste1.py

--- n8n_pipeteste1.py
+++ n8n_pipeteste1.py
@@ -1,11 +1,12 @@
 from typing import List, Union, Generator, Iterator, Optional
 import requests
 import json
+import time
 
 class Pipeline:
     def __init__(self):
-        self.name = "AIM - N8N Agent Pipeline2-teste"
-        self.api_url = "https://n8n.autointmind.com/webhook-test/62f78f96-6cae-4cfd-985d-27d2da8fd8b5"  # Set correct hostname
+        self.name = "AIM - N8N Agent Pipeline2"
+        self.api_url = "https://n8n.autointmind.com/webhook-teste/62f78f96-6cae-4cfd-985d-27d2da8fd8b5"  # Set correct hostname
         self.verify_ssl = True
         self.debug = False
 
@@ -39,12 +40,18 @@ class Pipeline:
             print(f"pipe: {__name__} - received message from user: {user_message}")
 
         try:
-            response = requests.get(self.api_url, timeout=3000)
+            response = requests.get(self.api_url, timeout=10)
             response.raise_for_status()
-            return response.json()
+            start_time = time.time()
+            while time.time() - start_time < 60:  # aguardar por até 60 segundos
+                if response.status_code == 200:
+                    if response.json():
+                        return response.json()
+                time.sleep(1)  # aguardar por 1 segundo antes de verificar novamente
+            return "Timeout error"
         except requests.exceptions.HTTPError as e:
             print(f"HTTP Error: {e}")
-            return f"Workflow request failed with status code:99"
+            return f"Workflow request failed with status code: {e.response.status_code}"
         except requests.exceptions.ConnectionError as e:
             print(f"Connection Error: {e}")
             return "Error connecting to the API"
@@ -56,3 +63,4 @@ class Pipeline:
             return "Error parsing JSON"
 
 retorno = Pipeline().pipe("mensagem", "modelo", [], {})
+print(retorno)

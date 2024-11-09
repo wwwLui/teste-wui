777c93bb25571620d559b27a424ab14878cc12bc
uid=0(wwwLui) <39214770+wwwLui@users.noreply.github.com>
2024-11-08 22:27:34 -0400

    Update and rename wqwqwq.py to qxw.py

--- qxw.py
+++ qxw.py
@@ -45,33 +45,15 @@ class Pipeline:
         return body
 
     def pipe(self, user_message: str, model_id: str, messages: List[dict], body: dict) -> Union[str, Generator, Iterator]:
-        # This is where you can add your custom pipelines like RAG.
-        print(f"pipe: {__name__}")
-        
-        if self.debug:
-            print(f"pipe: {__name__} - received message from user: {user_message}")
-        
-        # This function triggers the workflow using the specified API.
-        headers = {
-            #'Authorization': f'Bearer {self.api_key}',
-            'Content-Type': 'application/json'
-        }
-      #  data = {
-      #      "inputs": {"prompt": user_message},
-       #     "user": body["user"]["email"]
-    #    }
-
         response = requests.get(self.api_url, verify=self.verify_ssl)
-        print("======================>")
-        print(response)
         if response.status_code == 200:
             try:
-                for line in response.iter_lines():
-                    if line:
-                        # Decodifica cada linha como string e retorna diretamente
-                        yield line.decode('utf-8')
+                json_data = response.json()
+                if 'output' in json_data:
+                    return json_data['output']
+                else:
+                    return "No output key found in the response."
             except json.JSONDecodeError as e:
-                print(f"Failed to parse JSON from line. Error: {str(e)}")
-                yield "Error in JSON parsing."
+                return "Error in JSON parsing."
         else:
-            yield f"Workflow request failed with status code: {response.status_code}"
+            return f"Workflow request failed with status code: {response.status_code}"
